e1b418fb53d9a8065aa5bd484ca7ffdec4351163
Mario Flores <umlouco@gmail.com>
2017-10-25 17:11:43 +0000

    fixes to the proxy test system

--- Proxy.php
+++ Proxy.php
@@ -4,23 +4,26 @@ namespace MarioFlores\Proxy;
 
 use MarioFlores\Proxy\Database;
 use MarioFlores\Proxy\Scrape;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\RequestException;
 
 class Proxy {
-
+    public $output = false; 
     public function getProxy() {
         $this->checkStock();
         $tested = false;
-        $client = new Client();
         while ($tested == false) {
             $proxy = Database::orderByRaw("RAND()")->first();
-            $proxy = $proxy; 
-            $tested = $this->test($proxy); 
-            if($tested == false){
-                Database::destroy($proxy->id); 
+            $proxy = $proxy;
+            $tested = $this->test($proxy);
+            if ($tested == false) { 
+                Database::destroy($proxy->id);
+                $this->output('Deleted proxy '); 
+            } else {
+                return (array) $proxy;
             }
-            else{
-                return (array)$proxy; 
-            }
+           
         }
     }
 
@@ -29,11 +32,21 @@ class Proxy {
         if ($stock < 50) {
             $scrape = new Scrape;
             $scrape->scrape();
-            Database::insert($scrpe->proxys);
+            Database::insert($scrape->proxys);
         }
     }
 
     function test($proxy) {
+        $client = new Client([
+            'headers' => [
+                'User-Agent' => "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",
+                'Accept-Language' => "en-US,en;q=0.5"
+            ],
+            'timeout' => 60,
+            'cookies' => new \GuzzleHttp\Cookie\CookieJar,
+            'http_errors' => false,
+            'allow_redirects' => true
+        ]);
         try {
             $response = $client->request('GET', 'http://agency-systems.com/home/proxys/', [
                 'proxy' => 'tcp://' . $proxy->ip . ':' . $proxy->port,
@@ -43,26 +56,37 @@ class Proxy {
             if ($response->getStatusCode() == 200) {
                 $header = $response->getHeader('Content-Type');
                 if ($header[0] != 'application/json') {
+                    $this->output('Response was not json '); 
                     return false;
                 } else {
                     $resposta = $response->getBody()->getContents();
                     if (empty($resposta)) {
+                        $this->output('Response was empty '); 
                         return false;
                     } else {
                         $json = $resposta;
                         $resposta = json_decode($json);
                         if ($resposta->ip == '62.28.58.182') {
+                            $this->output('Proxy is transparent '); 
                             return false;
                         } else {
+                            $this->output('the proxy is ok'); 
                             return true;
                         }
                     }
                 }
             } else {
+                $this->output('Response was different then 2000 '); 
                 return false;
             }
         } catch (RequestException $ex) {
             return false;
+        }
+    }
+    
+    function output($message){
+        if($this->output === true){
+            echo $message."<br /> \n"; 
         }
     }
 
--- readme.md
+++ readme.md
@@ -4,3 +4,32 @@ Proxy rotator
 Keeps a list of proxies alive and updated. 
 Uses Eloquent to manege a simple database table withe the proxies. 
 Scrapes some sites as a proxy source.
+
+# Needs database table to store proxy information 
+```mysql
+--
+-- Table structure for table `proxy`
+--
+
+CREATE TABLE `proxy` (
+  `id` int(11) NOT NULL,
+  `ip` varchar(256) NOT NULL,
+  `port` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--
+-- Indexes for table `proxy`
+--
+ALTER TABLE `proxy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `proxy`
+--
+ALTER TABLE `proxy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+```
